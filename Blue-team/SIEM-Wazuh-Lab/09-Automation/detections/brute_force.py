ff2bc748921fa5971dbb5f64b2ea437b9f78dec2
ZeroDayTom <tom.staub99@sfr.fr>
2026-01-28 22:06:35 +0100

    Avoid duplicate alerts brute_force file

--- Blue-team/SIEM-Wazuh-Lab/09-Automation/detections/brute_force.py
+++ Blue-team/SIEM-Wazuh-Lab/09-Automation/detections/brute_force.py
@@ -5,13 +5,18 @@ FAILED_THRESHOLD = 5
 TIME_WINDOW_MINUTES = 5
 
 def detect_bruteforce(events):
+
     alerts = []
     events_by_ip = defaultdict(list)
+    alerted_ips = set()  # Avoid duplicate alerts
 
     for event in events:
         events_by_ip[event["source_ip"]].append(event)
 
     for ip, ip_events in events_by_ip.items():
+        if ip in alerted_ips:  # Skip if already alerted
+            continue
+            
         ip_events.sort(key=lambda x: x["timestamp"])
 
         for i in range(len(ip_events)):
@@ -28,6 +33,7 @@ def detect_bruteforce(events):
                     "first_seen": window[0]["timestamp"].isoformat(),
                     "last_seen": window[-1]["timestamp"].isoformat()
                 })
+                alerted_ips.add(ip)  # Indicate as alerted
                 break
 
     return alerts
